96124f9f96dfdd18a00e8f28c3960c99d813a015
Muhammad Ali <afsar4540@gmail.com>
2021-02-14 23:09:31 +0500

    Lessons file R-programming by alnafi

---
+++ lessons01_datatypesOps.R
@@ -0,0 +1,14 @@
+
+
+#Objects are basically of five types also called atomic
+#01 character (aA to zZ)
+#02 numeric (real numbers) (also decimal numbs)
+#03 integer (without decimal)
+#04 complex (having imaginary num i)
+#05 logical (true/false)
+
+#most basic object is vector which have objects of same class
+
+x <- 1
+print (x)
+
